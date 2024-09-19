e035d06189de621610aba8efd9064a6e1224e77a
waltujo <henrialqui@gmail.com>
2024-09-19 15:42:40 -0300

    Deletar o usuario com DELETE

--- src/main/java/com/waltujo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/waltujo/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,5 +57,13 @@ public class UserResource {
                 .toUri();
         
     	return ResponseEntity.created(uri).build();
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+      
+    	service.delete(id);
+    			
+        return ResponseEntity.noContent().build();
     }
 }
