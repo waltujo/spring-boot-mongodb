d051654b02e289636f429cd31483f259ad71a2f9
waltujo <henrialqui@gmail.com>
2024-09-25 14:58:03 -0300

    Endpoint oara retornas posts do usuario

--- src/main/java/com/waltujo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/waltujo/workshopmongo/resources/UserResource.java
@@ -10,15 +10,16 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.waltujo.workshopmongo.domain.Post;
 import com.waltujo.workshopmongo.domain.User;
 import com.waltujo.workshopmongo.dto.UserDTO;
 import com.waltujo.workshopmongo.services.UserService;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -76,5 +77,13 @@ public class UserResource {
     	obj = service.update(obj);
     	
     	return ResponseEntity.noContent().build();
+    }
+    
+    @GetMapping("/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+      
+    	User obj = service.findById(id);
+        		
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
