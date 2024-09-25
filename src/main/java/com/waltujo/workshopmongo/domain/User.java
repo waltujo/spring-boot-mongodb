b979d2eb92d3591bd2d7217986b0a415bb632ab8
waltujo <henrialqui@gmail.com>
2024-09-25 14:52:07 -0300

    Referênciando os posts do usuario

--- src/main/java/com/waltujo/workshopmongo/domain/User.java
+++ src/main/java/com/waltujo/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.waltujo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String Name;
 	private String Email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>(); 
 	
 	public User() {
 		
@@ -54,7 +60,15 @@ public class User implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
 
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
